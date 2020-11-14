177d52b63d4613645c2d3e8c385115c59975ff05
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2020-11-14 15:13:59 +0100

    initial commit

---
+++ spec/decidim-cli_spec.cr
@@ -0,0 +1,25 @@
+require "./spec_helper"
+
+describe Decidim::Cli do
+  context "should display" do
+    it "ruby version" do
+      fail "not implemented ruby version"
+    end
+
+    it "decidim version" do
+      fail "not implemented decidim version"
+    end
+
+    it "gems list with path and branchs" do
+      fail "not implemented gems list with path and branchs"
+    end
+
+    it "current git branch" do
+      fail "not implemented current git branch"
+    end
+
+    it "forge name for the current decidim gem" do
+      fail "not implemented forge name"
+    end
+  end
+end
---
+++ src/decidim-cli.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `Decidim::Cli`
+module Decidim::Cli
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
